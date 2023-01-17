3ecb2900f3a365d454974d74d5133c4754dd1963
KellyMart8 <110624872+KellyMart8@users.noreply.github.com>
2023-01-17 07:50:37 -0600

    Agregar archivos de proyecto.

---
+++ prueba diagnostica/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prueba_diagnostica
+{
+    internal class Class1
+    {
+        int cEmpleado, salario;
+
+        public int CEmpleado { get; set; }
+        public int Salario { get; set; }
+
+        public double InssL()
+        {
+            return (Salario * (0.07));
+        }
+
+        public double InssP()
+        {
+            return (Salario * (0.21));
+        }
+
+        //public void InssL()
+        //{
+        //    double iL = Salario * (0.07);
+        //}
+
+        //public void InssP()
+        //{
+        //    double iP = Salario * (0.21);
+        //}
+    }
+}
---
+++ prueba diagnostica/Program.cs
@@ -0,0 +1,25 @@
+﻿using prueba_diagnostica;
+
+Class1 c = new Class1();
+
+do
+{
+    Console.WriteLine("Ingrese la canridad de empleados");
+    c.CEmpleado = Convert.ToInt32(Console.ReadLine());
+    
+    for (int i = 0; i < c.CEmpleado; i++)
+    {
+
+        Console.WriteLine("El empleado " + (i+1) + ". Ingrese su saldo");
+        c.Salario = Convert.ToInt32(Console.ReadLine());
+
+        do
+        {
+            //Console.WriteLine("El salario es:" + c.Salario);
+            Console.WriteLine("El inss patronal es: " + c.InssP());
+            Console.WriteLine("El inss laboral es: " + c.InssL());
+        } while (c.Salario < 4799 && c.Salario >= 50000);
+
+    }
+
+} while (c.CEmpleado > 0 || c.CEmpleado <= 10);
